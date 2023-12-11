5bec0e93d7ed133a1a500c7ed5192417cc62cf5e
DeianGG <49059344+DeianGG@users.noreply.github.com>
2023-12-11 19:46:48 +0200

    Update registerauth.php

--- registerauth.php
+++ registerauth.php
@@ -1,11 +1,42 @@
 <?php
 session_start();
-
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require 'composer\vendor\autoload.php';
 if (isset($_POST['signup-submit'])) {
-    require 'connection.php';
+    include('connection.php');
 
+    
+
+    function sendver($email, $token){
+
+
+        $mail = new PHPMailer(true);                
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'deian.ageu1@gmail.com';                     //SMTP username
+            $mail->Password   = 'xwoa rntc imdn eees';                               //SMTP password
+            $mail->SMTPSecure = "tls";            //Enable implicit TLS encryption
+            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            $mail->setFrom('deian.ageu1@example.com', 'SlimSage');
+            $mail->addAddress($email);
+
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Confirmati adresa de email - Slim Sage';
+            $mail->Body    = "<h2>Confirmati adresa de email apasand pe link-ul de mai jos</h2><br/><br/><a href='http://localhost/verifymail.php?token=$token'>Apasati aici</a>";
+
+            $mail->send();
+    }
+    
     $password = $_POST['password'];
     $email = $_POST['email'];
+    $token = md5(rand());
+
+    sendver("$email", "$token");
+
 
         $sql = "SELECT email FROM users WHERE email=?";
         $stmt = mysqli_stmt_init($conn);
@@ -24,7 +55,7 @@ if (isset($_POST['signup-submit'])) {
                 header("Location: register.php?error=emailtaken");
                 exit();
             } else {
-                $sql = "INSERT INTO users (email, password, permission) VALUES (?, ?, 0 )";
+                $sql = "INSERT INTO users (email, password, permission, token, verified) VALUES (?, ?, 0, ?, 0)";
                 $stmt = mysqli_stmt_init($conn);
 
                 if (!mysqli_stmt_prepare($stmt, $sql)) {
@@ -32,12 +63,13 @@ if (isset($_POST['signup-submit'])) {
                     exit();
                 } else {
                     $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
-                    mysqli_stmt_bind_param($stmt, "ss", $email, $hashedPwd);
+                    mysqli_stmt_bind_param($stmt, "sss", $email, $hashedPwd, $token);
                     mysqli_stmt_execute($stmt);
 
                     $_SESSION['loggedin']=true;
                     $_SESSION['email']=$email;
                     $_SESSION['permission'] = 0;
+                    $_SESSION['verified']=false;
 
                     header("Location: index.php");
                     exit();
